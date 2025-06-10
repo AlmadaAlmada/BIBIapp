d58bd6fc89251a75ceb8f668dc242b6acdf30b04
Gabriel Cosato <gabrielcosato@gmail.com>
2025-06-09 21:08:33 -0300

    tela de alertas listando os alertas

--- frontend/my-app/src/pages/Alerta/index.tsx
+++ frontend/my-app/src/pages/Alerta/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { Text, View, Image, TextInput, Button, TouchableOpacity, Alert, ScrollView } from 'react-native';
 
@@ -12,13 +12,95 @@ import { ModalEdit } from "../../components/ModalEdit";
 import { BlurView } from 'expo-blur';
 import { ModalCreate } from "../../components/ModalCreate";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { useNavigation } from "@react-navigation/core";
+import { useFocusEffect, useNavigation } from "@react-navigation/core";
 import { SafeAreaView } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { listarAlertasComStatusBff } from "../bff/alertaBff";
 
 
 export default function Alerta() {
 
     const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+    const [idCarro, setidCarro] = useState<string | null>(null);
+
+    const [alertas, setAlertas] = useState<any[]>([]);
+
+
+    useFocusEffect(
+        useCallback(() => {
+            const buscaridCarro = async () => {
+                const idCarroSalvo = await AsyncStorage.getItem('idCarro');
+                setidCarro(idCarroSalvo);
+                console.log("CADE O ID DO CARRO NA TELA INICIAL??", idCarroSalvo);
+            };
+
+            buscaridCarro();
+        }, [])
+    );
+
+
+    const [uid, setUid] = useState<string | null>(null);
+
+
+    useFocusEffect(
+        useCallback(() => {
+            const buscarUid = async () => {
+                const uidSalvo = await AsyncStorage.getItem('uid');
+                setUid(uidSalvo);
+                console.log("CADE O ID DO USUARIO NA TELA INICIAL?", uidSalvo);
+            };
+
+            buscarUid();
+        }, [])
+    );
+
+    useEffect(() => {
+
+        if (!uid) return;
+        if (!idCarro) return;
+
+        const listarAlertasComStatus = async () => {
+            try {
+
+                const resposta = await listarAlertasComStatusBff(uid!, idCarro!);
+
+                console.log("Resposta dos cards do alerta com status:")
+                console.log(resposta);
+
+                if (resposta.sucesso && Array.isArray(resposta.alertas)) {
+                    resposta.alertas.forEach((alerta: { peca: any; dataUltimaTroca: any; status: any; kmRestante: any; mesesRestantes: any; }, index: number) => {
+                        console.log(`Alerta ${index + 1}`);
+                        console.log(`Peça: ${alerta.peca}`);
+                        console.log(`Última troca: ${alerta.dataUltimaTroca}`);
+                        console.log(`Status: ${alerta.status}`);
+                        console.log(`KM restante: ${alerta.kmRestante}`);
+                        console.log(`Meses restantes: ${alerta.mesesRestantes}`);
+                    });
+
+                    setAlertas(resposta.alertas);
+                }
+
+                if (resposta.sucesso) {
+
+                    console.log("alertas com status vieram perfeitos!")
+
+                } else {
+                    Alert.alert('Erro', resposta.mensagem);
+                }
+            } catch (error) {
+                Alert.alert('Erro', 'Não foi possível salvar o carro')
+            }
+        }
+
+        listarAlertasComStatus();
+    }, [uid, idCarro]);
+
+    function formatarData(data: string): string {
+        const objData = new Date(data);
+        if (isNaN(objData.getTime())) return "Data inválida";
+        return objData.toISOString().split("T")[0];
+    }
 
     return (
         <SafeAreaView style={style.container}>
@@ -35,77 +117,18 @@ export default function Alerta() {
 
                     <View style={style.formata}>
 
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
-
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
-
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
-
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
-
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
-
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
-
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
-
-                        <Card2
-                            imageLeft={Bola}
-                            title="Produto"
-                            subtitle="Próxima troca: XX/XX"
-                            subtitle2="Última troca: XX/XX"
-                            imageRight={Editar}
-                            imageRightStyle={{ width: 340, height: 330 }}>
-                        </Card2>
+                        {alertas.map((alerta, index) => (
+                            <View style={style.formata} key={index}>
+                                <Card2
+                                    imageLeft={Bola}
+                                    title={alerta.peca}
+                                    subtitle={`Próxima troca: ${alerta.mesesRestantes} meses`}
+                                    subtitle2={`Última troca: ${formatarData(alerta.dataUltimaTroca)}`}
+                                    imageRight={Editar}
+                                    bottomText={alerta.status}
+                                />
+                            </View>
+                        ))}
 
 
 
